eda49beed426294e480023f1b5e363294dac01dc
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-06-25 16:27:26 +0800

    Create loading screen while authenticating

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,7 +1,25 @@
+import { useState } from 'react';
+
+import { createUser } from '../util/auth';
 import AuthContent from '../components/Auth/AuthContent';
+import LoadingOverlay from '../components/ui/LoadingOverlay';
 
 function SignupScreen() {
-  return <AuthContent />;
+
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  async function signupHandler({ email, password }) {
+    setIsAuthenticating(true);
+    await createUser(email, password);
+    setIsAuthenticating(false);
+  }
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message={"Creating user..."} />
+  }
+
+  return <AuthContent onAuthenticate={signupHandler} />;
+
 }
 
 export default SignupScreen;
