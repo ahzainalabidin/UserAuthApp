bbbb71ffbebb597f824446b9a34d63e5a11e0ce4
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-06-27 16:09:21 +0800

    Access message from database

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,12 +1,27 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 function WelcomeScreen() {
+
+  const [fetchedMessage, setFetchedMessage] = useState('');
+
+  useEffect(() => {
+    axios.get(
+      'https://userauthapp-40f3a-default-rtdb.asia-southeast1.firebasedatabase.app/message.json'
+    ).then((response) => {
+      setFetchedMessage(response.data);
+    });
+  }, []);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
+
 }
 
 export default WelcomeScreen;
